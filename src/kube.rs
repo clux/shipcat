cc6da078512df1a1ad9696d905309254340976a6
Eirik Albrigtsen <eirik.albrigtsen@babylonhealth.com>
2018-02-10 12:37:23 +0000

    shipcat shell

--- src/kube.rs
+++ src/kube.rs
@@ -1,6 +1,6 @@
 use super::{Result, Manifest};
 
-fn kubeout(args: Vec<String>) -> Result<()> {
+fn kexec(args: Vec<String>) -> Result<()> {
     use std::process::Command;
     let s = Command::new("kubectl").args(&args).status()?;
     if !s.success() {
@@ -8,18 +8,32 @@ fn kubeout(args: Vec<String>) -> Result<()> {
     }
     Ok(())
 }
+fn kout(args: Vec<String>) -> Result<String> {
+    use std::process::Command;
+    let s = Command::new("kubectl").args(&args).output()?;
+    let out : String = String::from_utf8_lossy(&s.stdout).into();
+    // kubectl keeps returning opening and closing apostrophes - strip them:
+    if out.len() > 2 && out.chars().next() == Some('\'') {
+        let res = out.split('\'').collect::<Vec<_>>()[1];
+        return Ok(res.into());
+    }
+    Ok(out)
+
+}
 
 // NB: location not used
 // assumed to have been sanity checked before!
 pub fn rollout(region: &str, tag: &str, mf: &Manifest) -> Result<()> {
     // further sanity
     let confargs = vec!["config".into(), "current-context".into()];
-    kubeout(confargs)?;
+    kexec(confargs)?;
 
     let env = mf._namespace.clone();
     let loc = mf._location.clone();
     assert!(region.starts_with(&env));
     assert!(region.ends_with(&loc));
+
+    // TODO: check if access to roll out deployment!
 
     let mut img = mf.image.clone().unwrap();
     img.tag = Some(tag.into());
@@ -33,7 +47,7 @@ pub fn rollout(region: &str, tag: &str, mf: &Manifest) -> Result<()> {
         env.clone(),
     ];
     println!("kubectl {}", args.join(" "));
-    kubeout(args)?;
+    kexec(args)?;
 
     let rollargs = vec![
         "rollout".into(),
@@ -49,17 +63,63 @@ pub fn rollout(region: &str, tag: &str, mf: &Manifest) -> Result<()> {
         "-n".into(),
         env.into(),
     ];
-    match kubeout(rollargs.clone()) {
+    match kexec(rollargs.clone()) {
         Err(e) => {
             warn!("Rollout seems to hang - investigating");
             warn!("Got: {} from rollout command", e);
             info!("Checking pod status:");
-            kubeout(podargs)?;
+            kexec(podargs)?;
             bail!("rollout failed to succeed in 5minutes");
         }
         Ok(_) => {
             info!("rollout done!");
         }
     };
+    Ok(())
+}
+
+pub fn shell(mf: &Manifest, desiredpod: Option<u32>) -> Result<()> {
+    // TODO: check if access to shell in!
+
+    let env = mf._namespace.clone();
+    //let loc = mf._location.clone();
+
+    //kubectl get pods -l=app=$* -n $(ENV) -o jsonpath='{.items[*].metadata.name}'
+    let podargs = vec![
+        "get".into(),
+        "pods".into(),
+        format!("-l=app={}", mf.name.clone().unwrap()),
+        "-n".into(),
+        env.clone(),
+        "-o".into(),
+        "jsonpath='{.items[*].metadata.name}'".into(),
+    ];
+    let podsres = kout(podargs)?;
+    let pods = podsres.split(' ');
+    info!("Active pods: {:?}", podsres);
+
+    let mut num = 0;
+
+    for p in pods {
+        num += 1;
+        if let Some(pnr) = desiredpod {
+            if pnr != num {
+                debug!("Skipping pod {}", num);
+                continue;
+            }
+        }
+
+        info!("Shelling into {}", p);
+        //kubectl exec -n $(ENV) -it $$pod (bash || sh) ;\
+        let execargs = vec![
+            "exec".into(),
+            "-n".into(),
+            env.clone(),
+            "-it".into(),
+            p.into(),
+            "sh".into(),
+        ];
+        kexec(execargs)?;
+    }
     Ok(())
 }
