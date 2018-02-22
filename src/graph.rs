4a00469f6cd9ed076b7e88ae7245235b74396452
Eirik Albrigtsen <eirik.albrigtsen@babylonhealth.com>
2018-02-22 05:47:00 +0000

    allow graphing the full service directory
    limited output atm, but generating a graph is easy

--- src/graph.rs
+++ src/graph.rs
@@ -1,3 +1,7 @@
+use std::path::Path;
+use std::io::{self, Write};
+
+use walkdir::WalkDir;
 use petgraph::graph::{DiGraph, NodeIndex};
 use petgraph::dot::{Dot, Config};
 use serde_yaml;
@@ -7,11 +11,14 @@ use super::{Manifest, Dependency, Result};
 #[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct Node {
     pub name: String,
+    //pub image: String,
 }
 impl Node {
     fn new(mf: &Manifest) -> Self {
         Node {
-            name: mf.name.clone()
+            name: mf.name.clone(),
+            // image would be nice, but requires env override atm - should be global
+            //image: format!("{}", mf.image.clone().unwrap()),
         }
     }
 }
@@ -34,11 +41,12 @@ impl Edge {
 
 type CatGraph = DiGraph<Node, Edge>;
 
-
-fn contains_node(name: &str, graph: &CatGraph) -> Option<Node> {
-    for rn in graph.raw_nodes() {
-        if rn.weight.name == name {
-            return Some(rn.weight.clone());
+fn nodeidx_from_name(name: &str, graph: &CatGraph) -> Option<NodeIndex> {
+    for id in graph.node_indices() {
+        if let Some(n) = graph.node_weight(id) {
+            if n.name == name {
+                return Some(id);
+            }
         }
     }
     None
@@ -53,12 +61,10 @@ fn recurse_manifest(idx: NodeIndex, mf: &Manifest, graph: &mut CatGraph) -> Resu
         }
 
         // skip if node exists to avoid infinite loop
-        if let Some(n) = contains_node(&dep.name, &graph) {
-            // TODO: avoid duplicate add - need to get the index of the node
-            let depidx = graph.add_node(n);
+        if let Some(depidx) = nodeidx_from_name(&dep.name, &graph) {
+            trace!("Linking root node {} to existing node {}", mf.name, dep.name);
             graph.update_edge(idx, depidx, Edge::new(&dep));
-
-            debug!("Ignoring covered node {}", dep.name);
+            debug!("Stopping recursing - node {} covered", dep.name);
             continue;
         }
 
@@ -69,13 +75,13 @@ fn recurse_manifest(idx: NodeIndex, mf: &Manifest, graph: &mut CatGraph) -> Resu
 
         graph.update_edge(idx, depidx, Edge::new(&dep));
         recurse_manifest(depidx, &depmf, graph)?;
-
     }
 
     Ok(())
 }
 
-pub fn generate(service: &str, dot: bool) -> Result<()> {
+/// Generate dependency graph from an entry point
+pub fn generate(service: &str, dot: bool) -> Result<CatGraph> {
     let base = Manifest::basic(service)?;
 
 
@@ -91,6 +97,63 @@ pub fn generate(service: &str, dot: bool) -> Result<()> {
     else {
         println!("{}", serde_yaml::to_string(&graph)?);
     }
+    io::stdout().flush()?; // allow piping stdout elsewhere
 
-    Ok(())
+    Ok(graph)
+}
+
+/// Generate dependency graph from services directory
+///
+/// TODO: optionally filter around a node
+pub fn full(dot: bool) -> Result<CatGraph> {
+    let svcsdir = Path::new(".").join("services");
+    let svcs = WalkDir::new(&svcsdir)
+        .min_depth(1)
+        .min_depth(1)
+        .into_iter()
+        .filter_map(|e| e.ok())
+        .filter(|e| e.file_type().is_dir());
+
+    let mut graph : CatGraph = DiGraph::<_, _>::new();
+    for e in svcs {
+        let mut cmps = e.path().components();
+        cmps.next(); // .
+        cmps.next(); // services
+        let svccomp = cmps.next().unwrap();
+        let svcname = svccomp.as_os_str().to_str().unwrap();
+
+        debug!("Scanning service {:?}", svcname);
+
+        let mf = Manifest::basic(svcname)?;
+        let node = Node::new(&mf);
+        let idx = graph.add_node(node);
+
+        for dep in &mf.dependencies {
+            if dep.name == "core-ruby" || dep.name == "php-backend-monolith" {
+                debug!("Ignoring dependencies for non-shipcat monolith");
+                continue;
+            }
+            let subidx = if let Some(id) = nodeidx_from_name(&dep.name, &graph) {
+                trace!("Found dependency with existing node: {}", dep.name);
+                id
+            } else {
+                trace!("Found dependency new in graph: {}", dep.name);
+                let depmf = Manifest::basic(&dep.name)?;
+                let depnode = Node::new(&depmf);
+                let depidx = graph.add_node(depnode);
+                depidx
+            };
+            graph.update_edge(idx, subidx, Edge::new(&dep));
+        }
+    }
+
+    if dot {
+        println!("{:?}", Dot::with_config(&graph, &[Config::EdgeNoLabel]));
+    }
+    else {
+        println!("{}", serde_yaml::to_string(&graph)?);
+    }
+    io::stdout().flush()?; // allow piping stdout elsewhere
+
+    Ok(graph)
 }
