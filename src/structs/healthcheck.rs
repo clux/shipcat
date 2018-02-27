6916d55ce965a3acd8bf934f158f7c5110f35614
Eirik Albrigtsen <eirik.albrigtsen@babylonhealth.com>
2018-02-27 14:03:21 +0000

    docs

--- src/structs/healthcheck.rs
+++ src/structs/healthcheck.rs
@@ -2,6 +2,10 @@
 /// HealthCheck
 ///
 /// Designed for HTTP services for now
+/// Used to generate liveness and readiness probes in kubernetes
+///
+/// If we need complete control over these, consider writing a probes struct
+/// and making it only allowed if this is not present.
 #[derive(Serialize, Deserialize, Clone, Default)]
 pub struct HealthCheck {
     /// Where the health check is located
