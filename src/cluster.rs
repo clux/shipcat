55322f89b685c2cf81bb62a504be7fba7e1831f2
Eirik Albrigtsen <eirik.albrigtsen@babylonhealth.com>
2018-04-05 14:54:36 +0100

    reduce race conditions from cluster helm reconcile
    it was fetching secrets first, then potentially 10minutes later,
    upgrading a service that was already deployed during. this could
    downgrade a service using the version cached before.
    
    now we fetch secrets and the version in the worker thread.
    this is also faster.

--- src/cluster.rs
+++ src/cluster.rs
@@ -13,16 +13,16 @@ use super::{Result, Config, Manifest};
 ///
 /// Upgrades multiple services at a time using rolling upgrade in a threadpool.
 /// Ignores upgrade failures.
-pub fn helm_reconcile(conf: &Config, region: String) -> Result<()> {
-    mass_helm(conf, region, UpgradeMode::UpgradeWait)
+pub fn helm_reconcile(conf: &Config, region: String, numjobs: usize) -> Result<()> {
+    mass_helm(conf, region, UpgradeMode::UpgradeWait, numjobs)
 }
 
 /// Helm installs region (disaster recovery)
 ///
 /// Installs multiple services at a time in a threadpool.
 /// This upgrade mode does not wait so this should only be limited by k8s.
-pub fn helm_install(conf: &Config, region: String) -> Result<()> {
-    mass_helm(conf, region, UpgradeMode::UpgradeInstall)
+pub fn helm_install(conf: &Config, region: String, numjobs: usize) -> Result<()> {
+    mass_helm(conf, region, UpgradeMode::UpgradeInstall, numjobs)
 }
 
 
@@ -30,8 +30,8 @@ pub fn helm_install(conf: &Config, region: String) -> Result<()> {
 ///
 /// Returns the diffs only from all services across a region.
 /// Farms out the work to a thread pool.
-pub fn helm_diff(conf: &Config, region: String) -> Result<()> {
-    mass_helm(conf, region, UpgradeMode::DiffOnly)
+pub fn helm_diff(conf: &Config, region: String, numjobs: usize) -> Result<()> {
+    mass_helm(conf, region, UpgradeMode::DiffOnly, numjobs)
 }
 
 
@@ -40,46 +40,65 @@ pub fn helm_diff(conf: &Config, region: String) -> Result<()> {
 /// Reads secrets first, dumps all the helm values files,
 /// then helm {operation} all the services.
 /// This still might still use helm wait, but it does multiple services at a time.
-fn mass_helm(conf: &Config, region: String, umode: UpgradeMode) -> Result<()> {
-    let services = Manifest::available()?;
-    let mut manifests = vec![];
-    for svc in services {
+fn mass_helm(conf: &Config, region: String, umode: UpgradeMode, numjobs: usize) -> Result<()> {
+    let mut svcs = vec![];
+    for svc in Manifest::available()? {
         debug!("Scanning service {:?}", svc);
         let mf = Manifest::basic(&svc, conf, None)?;
         if !mf.disabled && !mf.external && mf.regions.contains(&region) {
-            // need a tera per service (special folder handling)
-            let tera = template::init(&svc)?;
-            let v = vault::Vault::default()?;
-            let mut compmf = Manifest::completed(&region, &conf, &svc, Some(v))?;
-            let regdefaults = conf.regions.get(&region).unwrap().defaults.clone();
-            compmf.version = Some(helm::infer_version(&svc, &regdefaults)?);
-            let dep = Deployment {
-                service: svc.into(),
-                region: region.clone(),
-                manifest: compmf,
-                render: Box::new(move |tmpl, context| {
-                    template::render(&tera, tmpl, context)
-                }),
-            };
-            // create all the values first
-            let hfile = format!("{}.helm.gen.yml", dep.service);
-            let mfrender = helm::values(&dep, Some(hfile.clone()), false)?;
-            manifests.push(mfrender);
+            svcs.push(svc);
         }
     }
 
-    let n_workers = 8;
-    let n_jobs = manifests.len();
+    let n_workers = numjobs;
+    let n_jobs = svcs.len();
     let pool = ThreadPool::new(n_workers);
     info!("Reconciling {} jobs using {} workers", n_jobs, n_workers);
 
     let (tx, rx) = channel();
-    for mf in manifests {
+    for svc in svcs {
+        // satisfying thread safety
         let mode = umode.clone();
-        let tx = tx.clone();
-        pool.execute(move|| {
-            let hfile = format!("{}.helm.gen.yml", mf.name); // as above
-            let res = helm::upgrade(&mf, &hfile, mode);
+        let reg = region.clone();
+        let regdefaults = conf.regions.get(&reg).unwrap().defaults.clone();
+        let config = conf.clone();
+
+        let tx = tx.clone(); // tx channel reused in each thread
+        pool.execute(move || {
+            let v = vault::Vault::default().unwrap(); // TODO:
+            let mut mf = Manifest::completed(&reg, &config, &svc, Some(v)).unwrap(); // TODO
+
+            let svc = mf.name.clone();
+            // need a tera per service (special folder handling)
+            let tera = template::init(&svc).unwrap(); // TODO
+
+            // get version now to limit race condition time
+            match helm::infer_version(&svc, &regdefaults) {
+                Ok(v) => mf.version = Some(v),
+                Err(e) => {
+                    return tx.send(Err(e)).expect("channel will be there waiting for the pool");
+                }
+            }
+
+            let dep = Deployment {
+                service: svc.into(),
+                region: reg,
+                manifest: mf,
+                render: Box::new(move |tmpl, context| {
+                    template::render(&tera, tmpl, context)
+                }),
+            };
+
+            // write values to file
+            let hfile = format!("{}.helm.gen.yml", dep.service);
+            match helm::values(&dep, Some(hfile.clone()), false) {
+                Err(e) => {
+                    return tx.send(Err(e)).expect("channel will be there waiting for the pool");
+                },
+                _ => {},
+            };
+            // upgrade
+            let res = helm::upgrade(&dep.manifest, &hfile, mode);
             tx.send(res).expect("channel will be there waiting for the pool");
         });
     }
