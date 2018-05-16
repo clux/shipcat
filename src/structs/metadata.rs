65bc93379d56abb1b1047cec4bf9a33944ebb843
gleaver <guy@guyleaver.co.uk>
2018-05-16 12:29:54 +0100

    [ADP-66] - Manifest contacts should contain both a username and a slack handle (#65)

--- src/structs/metadata.rs
+++ src/structs/metadata.rs
@@ -1,6 +1,15 @@
 use super::traits::Verify;
 use super::{Config, Result};
 
+/// Contact data
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub struct Contact {
+    /// Free text name
+    pub name: String,
+    /// Slack handle
+    pub slack: String,
+}
+
 /// Metadata for a service
 #[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct Metadata {
@@ -10,7 +19,7 @@ pub struct Metadata {
     pub team: String,
     /// Contact person
     #[serde(default, skip_serializing_if = "Vec::is_empty")]
-    pub contacts: Vec<String>,
+    pub contacts: Vec<Contact>,
     /// Support channels
     #[serde(default, skip_serializing_if = "Vec::is_empty")]
     pub support: Vec<String>,
@@ -27,11 +36,14 @@ impl Verify for Metadata {
             bail!("Illegal team name {} not found in the config", self.team);
         }
         for cc in &self.contacts {
-            if !cc.starts_with("@") {
-                bail!("Contact need to start with the slack guid '@U...' - got {}", cc)
+            if cc.name.is_empty() {
+                bail!("Contact name cannot be empty")
             }
-            if cc.contains("|") {
-                bail!("Contact must be a slack user id - got {}", cc)
+            if !cc.slack.starts_with("@") {
+                bail!("Contact slack handle needs to start with the slack guid '@U...' - got {}", cc.slack)
+            }
+            if cc.slack.contains("|") {
+                bail!("Contact slack user id invalid - got {}", cc.slack)
             }
         }
 
