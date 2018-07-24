0628bbd03c17b4b73952b0efab9783e8756e699a
Florent Delannoy <florent.delannoy@babylonhealth.com>
2018-07-24 12:29:52 +0100

    [DEVOPS-3519] Bye-bye type safety! (#126)

--- src/structs/sidecar.rs
+++ src/structs/sidecar.rs
@@ -3,15 +3,8 @@ use super::{Resources};
 #[derive(Serialize, Deserialize, Clone)]
 #[serde(rename_all = "lowercase")]
 pub struct Sidecar {
-  pub name: SidecarName,
+  pub name: String,
 
   #[serde(skip_serializing_if = "Option::is_none")]
   pub resources: Option<Resources>,
-}
-
-#[derive(Serialize, Deserialize, Clone)]
-#[serde(rename_all = "lowercase")]
-pub enum SidecarName {
-  /// Redis sidecar
-  Redis,
 }
