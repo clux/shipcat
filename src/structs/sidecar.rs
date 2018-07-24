b9e428adc02fdcf2c87230a4abca50d70873d3b5
Florent Delannoy <florent.delannoy@babylonhealth.com>
2018-07-24 12:50:57 +0100

    [DEVOPS-3519] Add version field for sidecars (#127)

--- src/structs/sidecar.rs
+++ src/structs/sidecar.rs
@@ -1,9 +1,12 @@
 use super::{Resources};
 
 #[derive(Serialize, Deserialize, Clone)]
-#[serde(rename_all = "lowercase")]
+#[serde(deny_unknown_fields, rename_all = "lowercase")]
 pub struct Sidecar {
   pub name: String,
+
+  #[serde(skip_serializing_if = "Option::is_none")]
+  pub version: Option<String>,
 
   #[serde(skip_serializing_if = "Option::is_none")]
   pub resources: Option<Resources>,
