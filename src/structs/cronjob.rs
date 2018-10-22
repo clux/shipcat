5d965b426c66d669aad5e1a21e04a11dcc72c58c
Eirik Albrigtsen <eirik.albrigtsen@babylonhealth.com>
2018-10-22 14:17:27 +0100

    either set extraEnv or env in cronJob

--- src/structs/cronjob.rs
+++ src/structs/cronjob.rs
@@ -1,3 +1,5 @@
+use regex::Regex;
+
 use structs::resources::Resources;
 use structs::traits::Verify;
 use super::EnvVars;
@@ -27,19 +29,43 @@ pub struct CronJob {
     #[serde(default, skip_serializing_if = "Option::is_none")]
     pub resources: Option<Resources<String>>,
 
-    /// Image to use for cron job
     /// Extra environment variables
+    ///
+    /// If this is used, rather than `env`, then all main environment variables
+    /// are inherited (merged) in the chart.
     #[serde(default, skip_serializing_if = "EnvVars::is_empty")]
     pub extraEnv: EnvVars,
+
+    /// Environment variables
+    ///
+    /// If this is used, rather than `extraEnv`, then the job ONLY gets these
+    /// environment variables in the chart.
+    #[serde(default, skip_serializing_if = "EnvVars::is_empty")]
+    pub env: EnvVars,
 }
 
 
 impl Verify for CronJob {
     fn verify(&self, conf: &Config) -> Result<()> {
+        let re = Regex::new(r"^[0-9a-z\-]{1,50}$").unwrap();
+        if !re.is_match(&self.name) {
+            bail!("Please use a short, lower case cron job names with dashes");
+        }
+        // TODO: version verify
         self.extraEnv.verify(conf)?;
-        // TODO: name verify
+        self.env.verify(conf)?;
+
         if let Some(ref r) = &self.resources {
             r.verify(conf)?;
+        }
+        if self.image.is_some() && self.version.is_none() {
+            bail!("Cannot specify image without specifying version in CronJob")
+        }
+        if self.version.is_some() && self.image.is_none() {
+            bail!("Cannot specify the version without specifying an image in CronJob")
+        }
+        if !self.env.is_empty() && !self.extraEnv.is_empty() {
+            bail!("Cannot specify more than one of env and extraEnv in CronJob")
         }
 
         Ok(())
