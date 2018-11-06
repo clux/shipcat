e12641e980105c871d3f0a61191206ed3627b9eb
Eirik Albrigtsen <eirik.albrigtsen@babylonhealth.com>
2018-11-06 10:32:32 +0000

    fix mienikube setup - fix template

---
+++ examples/start.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -ex
+
+kubectl config set-context --cluster=minikube --user=minikube --namespace=apps minikube
+kubectl create namespace apps
+
+kubectl apply -f tiller.yml
+
+docker run --cap-add=IPC_LOCK -e 'VAULT_DEV_ROOT_TOKEN_ID=myroot' -e 'VAULT_DEV_LISTEN_ADDRESS=0.0.0.0:8200' -p 8200:8200 -d --rm --name vault vault:0.11.3
+export VAULT_ADDR=http://127.0.0.1:8200
+export VAULT_TOKEN=myroot
+
+sleep 5 # wait for vault
+vault secrets disable secret
+vault secrets enable -version=1 -path=secret kv
+
+sleep 10 # wait for tiller
+helm --tiller-namespace=apps install --set postgresqlPassword=pw,postgresqlDatabase=webapp -n=webapp-pg stable/postgresql
+vault write secret/minikube/webapp/DATABASE_URL value=postgres://postgres:pw@webapp-pg-postgresql.apps/webapp
