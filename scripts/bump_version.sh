7d3bd19b1fbbf09f358452f7a83d29b46efae292
Jose Alban <jose.alban@babylonhealth.com>
2018-12-13 11:31:21 +0000

    [DEVP-334] some love for macos users

--- scripts/bump_version.sh
+++ scripts/bump_version.sh
@@ -5,11 +5,15 @@
 # Use from shipcat root directory (git root)
 #
 
+set -eo pipefail
+
 if [ -z "${1}" ] || [ -z "${2}" ]; then
   echo "Usage: $0 <current> <next>"
   echo "Example: $0 0.77.0 0.78.0"
   exit 1
 fi
+
+set -xu
 
 if ! git grep -c "${1}" > /dev/null; then
   echo "The version '${1}' doesn't appear to be correct."
@@ -17,8 +21,17 @@ if ! git grep -c "${1}" > /dev/null; then
   exit 1
 fi
 
+SEDCLI="sed"
+if [ $(uname) == "Darwin" ]; then
+  SEDCLI="gsed"
+  if ! which $SEDCLI 2>&1 >/dev/null; then
+    echo "We rely on gnu-compatible sed, sorry mac users: `brew install gnu-sed`"
+    exit 1
+  fi  
+fi
+
 function do_replace() {
-  find . -maxdepth 2 -mindepth 2 -name "*.toml" -print0 | xargs -0 sed -i "s/${1}/${2}/g"
+  find . -maxdepth 2 -mindepth 2 -name "*.toml" -print0 | xargs -0 $SEDCLI -i "s/${1}/${2}/g"
 }
 
 do_replace "${1}" "${2}"
