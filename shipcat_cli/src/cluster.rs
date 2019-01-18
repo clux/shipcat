4bb33feef66de6536c560b8e2788501ebcf7a6a4
Jose Alban <jose.alban@babylonhealth.com>
2019-01-18 12:16:09 +0000

    syntax optimise

--- shipcat_cli/src/cluster.rs
+++ shipcat_cli/src/cluster.rs
@@ -49,7 +49,7 @@ fn crd_reconcile(svcs: Vec<String>, config: &Config, region: &Region, n_workers:
     // Make sure config can apply first
     let applycfg = if let Some(ref crs) = &region.customResources {
         // special configtype detected - re-populating config object
-        Config::new(crs.shipcatConfig.clone(), &region.name).unwrap().0
+        Config::new(crs.shipcatConfig.clone(), &region.name)?.0
     } else {
         config.clone()
     };
