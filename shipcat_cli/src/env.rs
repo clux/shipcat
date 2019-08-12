fe3ad82773fea55b2588cc78411c3338076665f0
Rhys Stansfield <rhys.m.stansfield@gmail.com>
2019-08-12 11:02:18 +0100

    [OB-9] Add warn when using mocked secrets (#258)

--- shipcat_cli/src/env.rs
+++ shipcat_cli/src/env.rs
@@ -3,6 +3,7 @@ use super::{Result, Config, Region};
 /// Print exports to source from a shell
 pub fn print_bash(svc: &str, conf: &Config, reg: &Region, mock: bool) -> Result<()> {
     let mf = if mock {
+        warn!("Using mocked values for secrets. Use `-s` to resolve secrets.");
         shipcat_filebacked::load_manifest(&svc, &conf, &reg)?.stub(&reg)?
     } else {
         shipcat_filebacked::load_manifest(&svc, &conf, &reg)?.complete(&reg)?
